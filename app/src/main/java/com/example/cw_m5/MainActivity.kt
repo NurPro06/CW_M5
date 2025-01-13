cac29b0fa42190c168a2fc155ed86f9e19b0d984
NurPro06 <jakeevnurbolovich@gmail.com>
2025-01-13 15:59:49 +0600

    dcdcdcde

--- app/src/main/java/com/example/cw_m5/MainActivity.kt
+++ app/src/main/java/com/example/cw_m5/MainActivity.kt
@@ -9,9 +9,10 @@ import com.example.cw_m5.data.LoveModel
 import com.example.cw_m5.data.RetrofitInstance
 import com.example.cw_m5.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val api = RetrofitInstance.api
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -26,33 +27,28 @@ class MainActivity : AppCompatActivity(){
             }
 
             api.getPercentage(
-                firstName = binding.etFirstName.text.toString(),
-                secondName = binding.etSecondName.text.toString(),
+                firstName = firstName,
+                secondName = secondName,
                 key = "5fdbc29d4fmshd7cda0e70bb4935p154483jsnb1ff05c79dc2",
-                host =  "love-calculator.p.rapidapi.com"
+                host = "love-calculator.p.rapidapi.com"
             ).enqueue(object : retrofit2.Callback<LoveModel> {
                 override fun onResponse(call: retrofit2.Call<LoveModel>, response: retrofit2.Response<LoveModel>) {
                     if (response.isSuccessful && response.body() != null) {
-                    val loveModel = response.body()!!
-                    val intent = Intent(this@MainActivity, ResultActivity::class.java)
+                        val loveModel = response.body()!!
+                        val intent = Intent(this@MainActivity, ResultActivity::class.java)
                         intent.putExtra("loveModel", loveModel)
                         startActivity(intent)
                     } else {
-                        Log.e("MainActivity", "API Error: ${response.code()} ${response.message()}()}")
+                        Log.e("MainActivity", "API Error: ${response.code()} ${response.message()}")
                         Toast.makeText(this@MainActivity, "Error: ${response.errorBody()?.string()}", Toast.LENGTH_SHORT).show()
                     }
                 }
 
                 override fun onFailure(p0: retrofit2.Call<LoveModel>, thr: Throwable) {
                     Log.e("MainActivity", "API Error: ${thr.message}", thr)
-                    Toast.makeText(this@MainActivity, "Error: ${thr.message}" , Toast.LENGTH_SHORT).show()
-
-
+                    Toast.makeText(this@MainActivity, "Error: ${thr.message}", Toast.LENGTH_SHORT).show()
                 }
-
             })
-            }
-
-    }
-
         }
+    }
+}
